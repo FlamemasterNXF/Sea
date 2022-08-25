af5d37a6976b0f343a22b5ed16a08a0c1741afad
FlamemasterNXF <flamemasternxf@gmail.com>
2022-08-24 21:34:46 -0400

    rework: Lexer

--- Sea/Program.cs
+++ Sea/Program.cs
@@ -8,8 +8,8 @@ namespace Sea
         public static void Main()
         {
             new ErrorConfig().ErrorSetup();
-            new Lexer().Lex("global gwa gwa int8 hat int16 hhhhhhhhh global + - * / % = [ { (");
-            new Parser().Parse(Lexer._tokens, true);
+            new Lexer().Lex("+ 123 += \"HELLO CHAT\" ++ gwa");
+            //new Parser().Parse(Lexer._tokens, true);
         }
     }
     internal class Message{
@@ -25,7 +25,39 @@ namespace Sea
             if(severity==2) _writeWithColor(ConsoleColor.Yellow, $"WARNING: {message}");
             if(severity==3){ _writeWithColor(ConsoleColor.Red, $"ERROR: {message}"); _errored = true; };
         }
-    };
+    }
+    internal class Position{
+        internal int index;
+        internal int lineIdx;
+        internal int line;
+        string text;
+        public Position(string txt, int idx, int lnIdx, int ln)
+        {
+            index = idx;
+            lineIdx = lnIdx;
+            line = ln;
+            text = txt;
+        }
+        internal void advance(string? currentChar=null){
+                ++this.index;
+                ++this.lineIdx;
+
+                if(currentChar == "\n"){
+                    ++this.line;
+                    this.lineIdx = 0;
+                }
+         }
+         internal void reverse(){
+            if(this.lineIdx-- == -1){ --this.line; this.lineIdx = 0; }
+            else{
+                --this.index;
+                --this.lineIdx;
+            }
+         }
+         internal Position copy(){
+            return new Position(text, index, lineIdx, line);
+         }
+    }
     internal class Lexer
     {
         internal readonly string[] _toks =
@@ -42,37 +74,150 @@ namespace Sea
             "break", "return", "continue"
         };
         internal static List<string> _tokens = new List<string>{};
-        internal static List<int> _tokIndexes = new List<int>{};
+        internal static List<string> _ids = new List<string>{};
+        internal static List<string> _numbers = new List<string>{};
+        internal static List<string> _strings = new List<string>{};
+        private char? currentChar;
+        private char? previousChar;
+        private bool shouldLex = true;
+    
         internal void Lex(string text, bool debug=false)
         {
-            StringBuilder keyBuilder = new StringBuilder();
-            foreach (string value in _keywords)
-            {
-                keyBuilder.Append(value);
-                keyBuilder.Append('|');
-            }
-            string keyBuilderS = keyBuilder.ToString().Remove(keyBuilder.Length-1);
-            StringBuilder tokBuilder = new StringBuilder();
-            foreach (string value in _toks)
-            {
-                tokBuilder.Append('\\');
-                tokBuilder.Append(value);
-                tokBuilder.Append('|');
-            }
-            string tokBuilderS = tokBuilder.ToString().Remove(tokBuilder.Length-1);
+            //string text = Regex.Replace( txt, @"\s", "" );
+            Position pos = new Position(text, 0, 0, 0);
+            string digits = "0123456789";
+            string digitsF = "0123456789.";
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            string lettersF = "abcdefghijklmnopqrstuvwxyz_";
             
-            string pattern = @"\b("+keyBuilderS+")\\b|"+tokBuilderS;
-            MatchCollection tokensDetected = Regex.Matches(text, pattern);
+            void advance(){
+                previousChar = currentChar;
+                pos.advance(currentChar.ToString());
+                if(pos.index < text.Length) currentChar = text[pos.index];
+                else shouldLex = false;
+            }
 
-            if(debug) Console.WriteLine("{0} matches found in:\n   {1}", tokensDetected.Count, text);
-
-            foreach (Match match in tokensDetected)
-            {
-                GroupCollection groups = match.Groups;
-                _tokens.Add(groups[0].Value);
-                _tokIndexes.Add(groups[0].Index);
-                if(debug) Console.WriteLine("'{0}' repeated at position {1}", groups[0].Value, groups[0].Index);
-
+            void makeNumber(){
+                string number = "";
+                bool isDecimal = false;
+                Position positionStart = pos.copy();
+                while(shouldLex && digitsF.Contains(currentChar.ToString())){
+                    if(currentChar == '.'){
+                        if(isDecimal){ Message._throw(3, "Cannot have multipe decimal points in a float."); break; }
+                        isDecimal = true;
+                        number += ".";
+                    }
+                    else{
+                        number += currentChar;
+                    }
+                    advance();
+                }
+                if(debug)Console.WriteLine($"Added {number}");
+                _numbers.Add(number);
+            }
+            void makeID(){
+                string id = "";
+                Position positionStart = pos.copy();
+                while(shouldLex && lettersF.Contains(currentChar.ToString())){
+                    id += currentChar;
+                    advance();
+                }
+                if(debug)Console.WriteLine($"Added {id}");
+                _ids.Add(id);
+            }
+            void makeString(){
+                string str = "";
+                Position positionStart = pos.copy();
+                advance();
+                while(shouldLex && currentChar != '"'){
+                    str += currentChar;
+                    advance();
+                }
+                advance();
+                if(debug)Console.WriteLine($"Added {str}");
+                _strings.Add(str);
+            }
+            void makeOp(char type){
+                if(!shouldLex) return;
+                if(type=='+'){
+                    advance();
+                    if(currentChar == '+'){ _tokens.Add("++"); advance(); }
+                    else if (currentChar == '='){ _tokens.Add("+=") ; advance(); }
+                    else{ _tokens.Add("+"); }
+                }
+                if(type=='-'){
+                    advance();
+                    if(currentChar == '-'){ _tokens.Add("--"); advance(); }
+                    else if(currentChar == '='){ _tokens.Add("+="); advance(); }
+                    else _tokens.Add("-"); 
+                }
+                if(type=='*'){
+                    advance();
+                    if(currentChar == '='){
+                        advance(); 
+                        if(currentChar == '='){ _tokens.Add("*=="); advance(); }
+                        else _tokens.Add("*=");
+                    }
+                    else _tokens.Add("*");
+                }
+                if(type=='/'){
+                    advance();
+                    if(currentChar == '='){ _tokens.Add("/="); advance(); }
+                    else _tokens.Add("/"); 
+                }
+                if(type=='%'){
+                    advance();
+                    if(currentChar == '='){ _tokens.Add("%="); advance(); }
+                    else _tokens.Add("%"); 
+                }
+                if(type=='='){
+                    advance();
+                    if(currentChar == '='){ _tokens.Add("=="); advance(); }
+                    else _tokens.Add("=");
+                }
+                if(type=='!'){
+                    advance();
+                    if(currentChar == '='){
+                        advance();
+                        if(currentChar == '='){ _tokens.Add("!=="); advance(); }
+                        else Message._throw(3, "Expected '==' after '!' ");
+                    }
+                    else Message._throw(3, "Expected '==' after '!' ");
+                }
+                if(type=='>'){
+                    advance();
+                    if(currentChar == '='){ _tokens.Add(">="); advance(); }
+                    else _tokens.Add(">");
+                }
+                if(type=='<'){
+                    advance();
+                    if(currentChar == '='){ _tokens.Add("<="); advance(); }
+                    else _tokens.Add(">");
+                }
+            }
+            
+            currentChar = text[0];
+            while(shouldLex && !Message._errored){
+                string workable = currentChar.ToString();
+                if(digits.Contains(workable)) makeNumber();
+                if(letters.Contains(workable)) makeID();
+                if(currentChar == '"') makeString();
+                if(currentChar == '+') makeOp('+');
+                if(currentChar == '-') makeOp('-');
+                if(currentChar == '*') makeOp('*');
+                if(currentChar == '/') makeOp('/');
+                if(currentChar == '%') makeOp('%');
+                if(currentChar == '=') makeOp('=');
+                if(currentChar == '!') makeOp('!');
+                if(currentChar == '>') makeOp('>');
+                if(currentChar == '<') makeOp('<');
+                if(currentChar == '(') _tokens.Add("(");
+                if(currentChar == ')') _tokens.Add(")");
+                if(currentChar == '[') _tokens.Add("[");
+                if(currentChar == ']') _tokens.Add("]");
+                if(currentChar == '{') _tokens.Add("{");
+                if(currentChar == '}') _tokens.Add("}");
+                if(currentChar == ' ') advance();
             }
         }
     }
@@ -92,7 +237,7 @@ namespace Sea
         internal void MakeObjectNode(){} //soon (for things within curly braces)
 
         private void simpleError(int i, int x){
-            if(ErrorConfig.errorFlags[i] > 1) Message._throw(ErrorConfig.errorFlags[i], $"{ErrorConfig.errorNames[i]} at {Lexer._tokIndexes[x]}");
+            if(ErrorConfig.errorFlags[i] > 1) Message._throw(ErrorConfig.errorFlags[i], $"{ErrorConfig.errorNames[i]} at {Lexer._tokens[x]}");
         }
 
         internal void Parse(List<string> toks, bool debug=false){
@@ -107,13 +252,13 @@ namespace Sea
                     List<string> strings = new List<string>(){toks[i]};
 
                     if(!Array.Exists(mods, x=> x== toks[i+1])){ 
-                        simpleError(1, Lexer._tokIndexes[i]);
+                        //simpleError(1, Lexer._tokIndexes[i]);
                         if(!Message._errored) strings.Add("simple");
                     }
                     else strings.Add(toks[i+1]);
 
                     if(!Array.Exists(types, x=> x== toks[i+2])){
-                        simpleError(2, Lexer._tokIndexes[i]);
+                        //simpleError(2, Lexer._tokIndexes[i]);
                         //no implicit casts for now
                     }
                     else strings.Add(toks[i+2]);
