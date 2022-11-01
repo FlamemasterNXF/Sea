4fe2c1af2cecf999c9cc4b27a5ad7bc115077fec
FlamemasterNXF <flamemasternxf@gmail.com>
2022-11-01 17:25:03 -0400

    feat: Tree Walking

--- Shore/Shore.cs
+++ Shore/Shore.cs
@@ -10,21 +10,40 @@ namespace Shore
             Console.Write("> ");
             var line = Console.ReadLine();
             if (string.IsNullOrWhiteSpace(line)) return;
+
+            var parser = new Parser(line);
+            var expression = parser.Parse();
             
-            var lexer = new Lexer(line);
-            while (true)
-            {
-                var token = lexer.NextToken();
-                if (token.Type == TokType.EOF)
-                    break;
-                Console.WriteLine($"{token.Type}: '{token.Text}'");
-                if(token.Value is not null) Console.WriteLine($"  {token.Value}");
-            }
+            LogNode(expression);
 
             // CONSOLE WINDOW CONTROL
-            // Console.WriteLine("Press C to close this window :)");
-            // try{ while(Console.ReadKey(true).Key != ConsoleKey.C){ Console.Read(); } }
-            // catch (Exception){ Console.WriteLine($"Console Window not found!"); }
+            //Console.WriteLine("Press C to close this window :)");
+            //try{ while(Console.ReadKey(true).Key != ConsoleKey.C){ Console.Read(); } }
+            //catch (Exception){ Console.WriteLine($"Console Window not found!"); }
+        }
+
+        static void LogNode(Node node, string indent = "", bool last = false)
+        {
+            var marker = last ? "└──" : "├──";
+
+            Console.Write(indent);
+            Console.Write(marker);
+            Console.Write(node.Type);
+
+            if (node is Token t && t.Value is not null)
+            {
+                Console.Write(" ");
+                Console.Write(t.Value);   
+            }
+            
+            Console.WriteLine();
+            indent += last ? "    " : "│   ";
+
+            var lastChild = node.GetChildren().LastOrDefault();
+            foreach (var child in node.GetChildren())
+            {
+                LogNode(child, indent, child == lastChild);
+            }
         }
     }
 
@@ -39,20 +58,28 @@ namespace Shore
         CloseParenToken,
         OpenParenToken,
         UnknownToken,
-        EOF
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression
     }
     
-    class Token
+    class Token : Node
     {
-        public TokType Type { get; }
-        public int Pos { get; }
-        public string Text { get; }
+        public override TokType Type { get; }
+
+        public override IEnumerable<Node> GetChildren()
+        {
+            return Enumerable.Empty<Node>();
+        }
+
+        public int Position { get; }
+        public string? Text { get; }
         public object? Value { get; }
 
-        public Token(TokType type, int pos, string text, object? value)
+        public Token(TokType type, int position, string? text, object? value)
         {
             Type = type;
-            Pos = pos;
+            Position = position;
             Text = text;
             Value = value;
         }
@@ -77,7 +104,7 @@ namespace Shore
         public Token NextToken()
         {
             if (_position >= _text.Length)
-                return new Token(TokType.EOF, _position, "\0", null);
+                return new Token(TokType.EndOfFileToken, _position, "\0", null);
 
             if (char.IsDigit(Current))
             {
@@ -112,6 +139,120 @@ namespace Shore
             if (Current == ')') return new Token(TokType.CloseParenToken, _position++, ")", null);
 
             return new Token(TokType.UnknownToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+    }
+
+    abstract class Node
+    {
+        public abstract TokType Type { get; }
+
+        public abstract IEnumerable<Node> GetChildren();
+    }
+
+    abstract class ExpressionNode : Node
+    {
+        
+    }
+
+    sealed class NumberExpressionNode : ExpressionNode
+    {
+        public Token NumberToken { get; }
+
+        public NumberExpressionNode(Token numberToken)
+        {
+            NumberToken = numberToken;
+        }
+
+        public override TokType Type => TokType.NumberExpression;
+        public override IEnumerable<Node> GetChildren()
+        {
+            yield return NumberToken;
+        }
+    }
+
+    sealed class BinaryExpressionNode : ExpressionNode
+    {
+        public ExpressionNode Left { get; }
+        public Token OperatorToken { get; }
+        public ExpressionNode Right { get; }
+
+        public BinaryExpressionNode(ExpressionNode left, Token operatorToken, ExpressionNode right)
+        {
+            Left = left;
+            OperatorToken = operatorToken;
+            Right = right;
+        }
+
+        public override TokType Type => TokType.BinaryExpression;
+        public override IEnumerable<Node> GetChildren()
+        {
+            yield return Left;
+            yield return OperatorToken;
+            yield return Right;
+        }
+    }
+    
+    class Parser
+    {
+        private readonly Token[] _tokens;
+        private int _position;
+
+        public Parser(string text)
+        {
+            var tokens = new List<Token>();
+
+            var lexer = new Lexer(text);
+            Token token;
+
+            do 
+            {
+                token = lexer.NextToken();
+                if (token.Type != TokType.WhitespaceToken && token.Type != TokType.UnknownToken) tokens.Add(token);
+            } while (token.Type != TokType.EndOfFileToken);
+
+            _tokens = tokens.ToArray();
+        }
+
+        private Token Peek(int offset)
+        {
+            var index = _position + offset;
+            if (index >= _tokens.Length) return _tokens[^1];
+            return _tokens[index];
+        }
+        
+        private Token Current => Peek(0);
+
+        private Token NextToken()
+        {
+            var current = Current;
+            _position++;
+            return current;
+        }
+        
+        private Token Match(TokType type)
+        {
+            if (Current.Type == type) return NextToken();
+            return new Token(type, Current.Position, null, null);
+        }
+
+        private ExpressionNode ParsePrimaryExpression()
+        {
+            var numberToken = Match(TokType.NumberToken);
+            return new NumberExpressionNode(numberToken);
+        }
+        
+        public ExpressionNode Parse()
+        {
+            var left = ParsePrimaryExpression();
+
+            while (Current.Type is TokType.PlusToken or TokType.DashToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpressionNode(left, operatorToken, right);
+            }
+
+            return left;
         }
     }
 }
