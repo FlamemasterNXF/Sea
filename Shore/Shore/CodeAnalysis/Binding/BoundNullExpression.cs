3d3934878ee516c444c05e3d5dca30b3a120f5e3
FlamemasterNXF <flamemasternxf@gmail.com>
2022-11-08 17:31:54 -0500

    fix(git): Add BoundNullExpression.cs to git

---
+++ Shore/Shore/CodeAnalysis/Binding/BoundNullExpression.cs
@@ -0,0 +1,10 @@
+﻿using Shore.CodeAnalysis.Symbols;
+
+namespace Shore.CodeAnalysis.Binding
+{
+    internal sealed class BoundNullExpression : BoundExpression
+    {
+        public override BoundNodeKind Kind => BoundNodeKind.NullExpression;
+        public override TypeSymbol Type => TypeSymbol.Null;
+    }
+}
