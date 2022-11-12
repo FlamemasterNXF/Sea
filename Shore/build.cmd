811128250a06b447c579915fd77efced2093c8cf
FlamemasterNXF <flamemasternxf@gmail.com>
2022-11-12 18:38:24 -0500

    chore: Remove Unneccesary .sln

--- Shore/build.cmd
+++ Shore/build.cmd
@@ -4,7 +4,7 @@ REM Vars
 set "SLNDIR=%~dp0"
 
 REM Restore + Build
-dotnet build "%SLNDIR%\Shore.sln" --nologo || exit /b
+dotnet build "%SLNDIR%\Shore" --nologo || exit /b
 
 REM Test
 dotnet test "%SLNDIR%\Shore.Tests" --nologo --no-build
