18f4d8d4734ff0425d54f6d4d4fa01444b747c63
FlamemasterNXF <flamemasternxf@gmail.com>
2022-11-13 13:12:21 -0500

    fix(lowerer): Lowering of While Statements

--- Shore/Shore/CodeAnalysis/Lowering/Lowerer.cs
+++ Shore/Shore/CodeAnalysis/Lowering/Lowerer.cs
@@ -77,16 +77,16 @@ namespace Shore.CodeAnalysis.Lowering
 
         protected override BoundStatement RewriteWhileStatement(BoundWhileStatement node)
         {
-            var checkLabel = GenerateLabel();
+            var bodyLabel = GenerateLabel();
 
-            var gotoCheck = new BoundGotoStatement(checkLabel);
+            var gotoContinue = new BoundGotoStatement(node.ContinueLabel);
+            var bodyLabelStatement = new BoundLabelStatement(bodyLabel);
             var continueLabelStatement = new BoundLabelStatement(node.ContinueLabel);
+            var gotoTrue = new BoundConditionalGotoStatement(bodyLabel, node.Condition);
             var breakLabelStatement = new BoundLabelStatement(node.BreakLabel);
-            var checkLabelStatement = new BoundLabelStatement(checkLabel);
-            var gotoTrue = new BoundConditionalGotoStatement(node.ContinueLabel, node.Condition);
 
-            var result = new BoundBlockStatement(ImmutableArray.Create(
-                gotoCheck, continueLabelStatement, node.Body, checkLabelStatement, gotoTrue, breakLabelStatement
+            var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(
+                gotoContinue, bodyLabelStatement, node.Body, continueLabelStatement, gotoTrue, breakLabelStatement
             ));
 
             return RewriteStatement(result);
